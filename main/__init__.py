9e67fa91df304ac9ed99c9101f70af63bc2242d8
BAHADIRking <b123qweasdzxc@gmail.com>
2021-08-23 14:19:19 +0300

    1.0.1

--- main/__init__.py
+++ main/__init__.py
@@ -1,150 +1,219 @@
-import os
+import os, sys
+
+import github.GithubException
 import pandas as pd
-import numpy as np
 from dotenv import load_dotenv
 from github import Github
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+
 class github_api:
 	def __init__(self):
 		load_dotenv()
 		self.api_key = os.environ["API_KEY"]
-		self.sql_key = os.environ["SQL_KEY"]
-
 
 	def get_api_followers_info(self):
-		github_api_info = Github(self.api_key)
-		return github_api_info.get_user().get_followers()
+		try:
+			github_api_info = Github(self.api_key)
+			github_api_info.get_user().login
+			return github_api_info.get_user().get_followers()
+
+		except github.GithubException:
+			raise "missing or incorrect API key"
 
 	def get_api_star_info(self):
-		github_api_info = Github(self.api_key)
-		github_api_info.get_user()
-		return github_api_info.get_user().get_repos(visibility="public")
+		try:
+			github_api_info = Github(self.api_key)
+			github_api_info.get_user().login
+			return github_api_info.get_user().get_repos(visibility="public")
+
+		except github.GithubException:
+			raise "missing or incorrect API key"
 
 	def get_user_info(self):
-		github_api_info = Github(self.api_key)
-		return github_api_info.get_user()
-	
+		try:
+			github_api_info = Github(self.api_key)
+			github_api_info.get_user().login
+			return github_api_info.get_user()
+
+		except github.GithubException:
+			raise "missing or incorrect API key"
+
 	def get_following_info(self):
-		github_api_info = Github(self.api_key)
-		return github_api_info.get_user().get_following()
+		try:
+			github_api_info = Github(self.api_key)
+			github_api_info.get_user().login
+			return github_api_info.get_user().get_following()
+
+		except github.GithubException:
+			raise "missing or incorrect API key"
+
+	def get_user_id(self):
+		try:
+			github_api_info = Github(self.api_key)
+			github_api_info.get_user().login
+			return github_api_info.get_user().id
+
+		except github.GithubException:
+			raise "missing or incorrect API key"
+
 
 github_api = github_api()
 
+
 def star():
-	df = pd.DataFrame(columns=["id","user_name","repo_name"])
+	df = pd.DataFrame(columns=["id", "user_name", "repo_name"])
 	for i in github_api.get_api_star_info():
 		for j in i.get_stargazers_with_dates():
 			df = df.append({
-				"id":j.user.id,
-				"user_name":j.user.login,
-				"repo_name":i.name
-				},ignore_index=True)
-	#df = df.sort_values(by=["id"])
-	#df.to_csv(f"{os.getcwd()}/star_info.csv",index=False)
-	#print(df)
+				"id": j.user.id,
+				"user_name": j.user.login,
+				"repo_name": i.name
+			}, ignore_index=True)
+	# df = df.sort_values(by=["id"])
+	# df.to_csv(f"{os.getcwd()}/star_info.csv",index=False)
+	# print(df)
 	##who scored how many stars
-	#a= df.groupby("user_name").count()["user_id"].sort_values(ascending=False)
+	# a= df.groupby("user_name").count()["user_id"].sort_values(ascending=False)
 	##which repo has how many stars
-	#b= df.groupby("repo_name").count()["user_id"].sort_values(ascending=False)
-	#print(a)
-	#print(b)
-	return  df
+	# b= df.groupby("repo_name").count()["user_id"].sort_values(ascending=False)
+	# print(a)
+	# print(b)
+	return df
+
+
 bahadir = github_api.get_user_info()
 
 
-def bahdir_csv():
-	bahadir_df = pd.DataFrame(columns=["id","user_name"])
-	bahadir_df = bahadir_df.append({
-		"id":bahadir.id,
-		"user_name":bahadir.login,
-		},ignore_index=True)
+class Functions:
 
-	return bahadir_df
+	def check_csv(self):
+		if fr"{github_api.get_user_id()}.csv" not in os.listdir():
+			self.bahdir_csv().to_csv(f"{os.getcwd()}/{github_api.get_user_id()}.csv", index=False)
+			self.github_data_csv().to_csv(f"{os.getcwd()}/{github_api.get_user_id()}_follower_info.csv", index=False)
+			self.bahadir_following_csv().to_csv(f"{os.getcwd()}/{github_api.get_user_id()}_following.csv", index=False)
+			self.star().to_csv(f"{os.getcwd()}/{github_api.get_user_id()}_stargazers_info.csv", index=False)
+			return os.listdir()
+		else:
+			Main_Menu().asil()
+
+	def bahdir_csv(self) -> pd.DataFrame:
+		bahadir_df = pd.DataFrame(columns=["id", "user_name"])
+		bahadir_df = bahadir_df.append({
+			"id": bahadir.id,
+			"user_name": bahadir.login,
+		}, ignore_index=True)
+		return bahadir_df
+
+	def github_data_csv(self) -> pd.DataFrame:
+		# create new dataframe
+		df2 = pd.DataFrame(columns=["id", "user_name"])
+		for i in github_api.get_api_followers_info():
+			df2 = df2.append({
+				"id": i.id,
+				"user_name": i.login,
+			}, ignore_index=True)
+
+		return df2
+
+	def bahadir_following_csv(self) -> pd.DataFrame:
+		df3 = pd.DataFrame(columns=["id", "user_name"])
+		for i in github_api.get_following_info():
+			df3 = df3.append({
+				"id": i.id,
+				"user_name": i.login,
+			}, ignore_index=True)
+		return df3
+
+	def star(self) -> pd.DataFrame:
+		df = pd.DataFrame(columns=["id", "user_name", "repo_name"])
+		for i in github_api.get_api_star_info():
+			for j in i.get_stargazers_with_dates():
+				df = df.append({
+					"id": j.user.id,
+					"user_name": j.user.login,
+					"repo_name": i.name
+				}, ignore_index=True)
+		return df
+
+	def create_csv(self, df: pd.DataFrame, file_name: str):
+		id = github_api.get_user_id()
+		options = {"1": f"{id}_follower_info.csv", "2": f"{id}_stargazers_info.csv", "3": f"{id}_following.csv"}
+
+		def path():
+			return os.path.join(os.getcwd(), options.get(file_name))
+
+		a = pd.read_csv(options.get(path()))
+		a = a.sort_values(by="id")
+		df = df.fillna(0)
+		df = df.sort_values(by="id")
+		if df.equals(a):
+			print("same")
+		elif pd.concat([a, df]).drop_duplicates(keep=False).empty:
+			print("same")
+		else:
+			df1 = pd.concat([a, df]).drop_duplicates(keep=False)
+			print(df1)
+			print("differencei")
 
 
+functions = Functions()
 
 
-def github_data_csv():
-#create new dataframe
-	df2 = pd.DataFrame(columns=["id","user_name"])
-	for i in github_api.get_api_followers_info():
-		df2 = df2.append({
-			"id":i.id,
-			"user_name":i.login,
-			},ignore_index=True)
-
-	return df2
-
-def bahadir_following_csv():
-	df3 = pd.DataFrame(columns=["id","user_name"])
-	for i in github_api.get_following_info():
-		df3 = df3.append({
-			"id":i.id,
-			"user_name":i.login,
-			},ignore_index=True)
-	return df3
-	
+# bahadir_csv_path = os.path.join(os.getcwd(), "bahadir.csv")
+#
+# github_data_csv_path = os.path.join(os.getcwd(), "github_data.csv")
+#
+# bahadir_following_csv_path = os.path.join(os.getcwd(), "following.csv")
+#
+# bahadir_star_info = os.path.join(os.getcwd(), "star_info.csv")
+#
+# create_csv(bahdir_csv(), bahadir_csv_path)
+# create_csv(github_data_csv(), github_data_csv_path)
+# create_csv(bahadir_following_csv(), bahadir_following_csv_path)
+# create_csv(star(), bahadir_star_info)
 
 
-def create_csv(df,file_name):
-	a=pd.read_csv(file_name)
-	a=a.sort_values(by="id")
-	df = df.fillna(0)
-	df=df.sort_values(by="id")
-	if df.equals(a):
+class Main_Menu:
+	try:
 
-		print("same")
-	elif pd.concat([a,df]).drop_duplicates(keep=False).empty:
-		print("same")
-	else:
-		df1 = pd.concat([a,df]).drop_duplicates(keep=False)
-		print(df1)
-		print("differencei")
+		def __init__(self):
 
-bahadir_csv_path =os.path.join(os.getcwd(),"bahadir.csv")
+			self.secenek = {
+				"1": functions.create_csv(functions.github_data_csv(),"1"),
+				"2": functions.create_csv(functions.star(),"2"),
+				"3": functions.create_csv(functions.bahadir_following_csv(),"3"),
+				"q": self.quit
+			}
 
-github_data_csv_path =os.path.join(os.getcwd(),"github_data.csv")
+		def menu_goster(self, ):
+			print("""
+menu
+1.followers
+2.star
+3.following
+exit to q
+    """)
 
-bahadir_following_csv_path =os.path.join(os.getcwd(),"following.csv")
+		def asil(self):
+			while True:
+				self.menu_goster()
+				secenek = input("sellect options: ")
+				dogrulama = self.secenek.get(secenek)
+				if dogrulama:
+					dogrulama()
+				else:
+					print(rf"{secenek} wrong options")
 
-bahadir_star_info = os.path.join(os.getcwd(),"star_info.csv")
+		def quit(self):
+			print("byyy")
+			sys.exit(0)
+
+	except FileNotFoundError:
+		pass
 
 
-create_csv(bahdir_csv(),bahadir_csv_path)
-create_csv(github_data_csv(),github_data_csv_path)
-create_csv(bahadir_following_csv(),bahadir_following_csv_path)
-create_csv(star(),bahadir_star_info)
-
-
-
-
-
-
-
-
-#for i in github_api.get_api_followers_info():
-#	print("_" * 50)
-#	try:
-#		print(i.id,
-#			  i.following,
-#			  i.email,
-#			  i.location,
-#			  i.followers,
-#			  i.following,
-#			  i.login,
-#			  [x.strip() for x in i.company[1:].split('@')] if i.company.split('@')[0] == '' else i.company.split('@'),
-#			  i.avatar_url,
-#			  i.bio,
-#			  i.name, sep="\n")
-#	except Exception:
-#		print(i.id,
-#			  i.email,
-#			  i.location,
-#			  i.followers,
-#			  i.following,
-#			  i.login,
-#			  i.company,
-#			  i.avatar_url,
-#			  i.bio,
-#			  i.name, sep="\n")
+if __name__ == "__main__":
+	print("hello")
+	functions.check_csv()
