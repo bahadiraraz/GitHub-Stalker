c61ab3a7dd83c1e6d26f6c3a36ee17d5ecc14b60
BAHADIRking <b123qweasdzxc@gmail.com>
2021-08-27 23:08:27 +0300

    update PEP-8 format

--- main/__main__.py
+++ main/__main__.py
@@ -10,6 +10,9 @@ pd.set_option("display.max_rows", None, "display.max_columns", None)
 class GithubApi:
 
 	def get_api_followers_info(self):
+		"""
+		This function get user followers info from GitHub REST API
+		"""
 		try:
 			github_api_info = Github(api_key)
 			github_api_info.get_user().login
@@ -19,6 +22,9 @@ class GithubApi:
 			raise "missing or incorrect API key"
 
 	def get_api_star_info(self):
+		"""
+		This function get user star info from GitHub REST API
+		"""
 		try:
 			github_api_info = Github(api_key)
 			github_api_info.get_user().login
@@ -28,6 +34,9 @@ class GithubApi:
 			raise "missing or incorrect API key"
 
 	def get_user_info(self):
+		"""
+		This function get user info from GitHub REST API
+		"""
 		try:
 			github_api_info = Github(api_key)
 			github_api_info.get_user().login
@@ -37,6 +46,9 @@ class GithubApi:
 			raise "missing or incorrect API key"
 
 	def get_following_info(self):
+		"""
+		This function get user following info from GitHub REST API
+		"""
 		try:
 			github_api_info = Github(api_key)
 			github_api_info.get_user().login
@@ -46,6 +58,9 @@ class GithubApi:
 			raise "missing or incorrect API key"
 
 	def get_user_id(self):
+		"""
+		This function get user id info from GitHub REST API
+		"""
 		try:
 			github_api_info = Github(api_key)
 			github_api_info.get_user().login
@@ -66,7 +81,13 @@ class Functions:
 		self.user_following_path = lambda: f"{os.getcwd()}\\csvs\\{github_api.get_user_id()}_following.csv"
 		self.user_star_path = lambda: f"{os.getcwd()}\\csvs\\{github_api.get_user_id()}_stargazers_info.csv"
 
-	def creates_csv(self):
+	def creates_csv(self) -> None:
+		"""
+		This function create csv files if not exist
+
+		:return: None
+		"""
+
 		try:
 			self.user_csv().to_csv(self.user_csv_path(), index=False)
 			self.user_followers_csv().to_csv(self.user_followers_path(), index=False)
@@ -76,31 +97,53 @@ class Functions:
 			os.makedirs(f"{os.getcwd()}\\csvs")
 			self.creates_csv()
 
-	def update_csv(self, file_name: str):
+	def update_csv(self, file_name: str) -> None:
+		"""
+		This function is used to update the csv files.
+
+		:param file_name: The name of the file to be updated.
+		:type file_name: str
+		:return: None
+		:rtype: None
+		"""
 		options = {self.user_csv_path(): self.user_csv(), self.user_followers_path(): self.user_followers_csv(),
-			 self.user_following_path(): self.user_following_csv(), self.user_star_path(): self.user_star_csv()}
+				   self.user_following_path(): self.user_following_csv(), self.user_star_path(): self.user_star_csv()}
 		options[file_name].to_csv(file_name, index=False)
 
-	def check_csv(self):
+	def check_csv(self) -> None:
+		"""
+		This function check csv file exist or not exist
+
+		:return: None
+		"""
 		if os.path.isfile(fr"{os.getcwd()}/csvs/{github_api.get_user_id()}.csv") == False:
 			print("creating csv file..")
 			self.creates_csv()
 			MainMenu().run()
-			return os.listdir()
 		else:
 			MainMenu().run()
 
 	def user_csv(self) -> pd.DataFrame:
+		"""
+		This function create user info csv.
+
+		:return: A dataframe
+		"""
 		user_df = pd.DataFrame(columns=["id", "user_name"])
 		user = github_api.get_user_info()
 		user_df = user_df.append({
 			"id": user.id,
 			"user_name": user.login,
 		}, ignore_index=True)
+
 		return user_df
 
 	def user_followers_csv(self) -> pd.DataFrame:
-		# create new dataframe
+		"""
+		This function create user followers info csv.
+
+		:return: A dataframe
+		"""
 		df = pd.DataFrame(columns=["id", "user_name"])
 		for i in github_api.get_api_followers_info():
 			df = df.append({
@@ -111,15 +154,46 @@ class Functions:
 		return df
 
 	def user_following_csv(self) -> pd.DataFrame:
+		"""
+		This function create user following info csv.
+
+		:return: A dataframe
+		"""
 		df = pd.DataFrame(columns=["id", "user_name"])
 		for i in github_api.get_following_info():
 			df = df.append({
 				"id": i.id,
 				"user_name": i.login,
 			}, ignore_index=True)
+
+		return df
+
+	def user_fork_csv(self) -> pd.DataFrame:
+		"""
+		This function create user repo forker info csv .
+
+		:returns: A dataframe
+		"""
+		df = pd.DataFrame(columns=["id", "user_name", "repo_name"])
+		for i in github_api.get_api_star_info():
+			if i.name == "copilot-preview":
+				pass
+			else:
+				for j in i.get_forks():
+					df = df.append({
+						"id": j.id,
+						"forker_name": j.full_name.split("/")[0],
+						"repo_name": i.name
+					}, ignore_index=True)
+
 		return df
 
 	def user_star_csv(self) -> pd.DataFrame:
+		"""
+		This function create user stargazers info dataframe csv.
+
+		:return: A dataframe
+		"""
 		df = pd.DataFrame(columns=["id", "user_name", "repo_name"])
 		for i in github_api.get_api_star_info():
 			if i.name == "copilot-preview":
@@ -135,11 +209,18 @@ class Functions:
 		return df
 
 	def compare_csv(self, df: pd.DataFrame, file_name: str) -> pd.DataFrame:
+		"""
+		Compares two csv files and prints the differences between them.
+
+		:param df: A dataframe.
+		:param file_name: A string.
+		:return: A dataframe.
+		"""
 		id = github_api.get_user_id()
 
 		options = {"1": f"{id}_follower_info.csv", "2": f"{id}_stargazers_info.csv", "3": f"{id}_following.csv"}
 
-		def path():
+		def path() -> os.path:
 			return os.path.join(os.getcwd() + "\\csvs", options.get(file_name))
 
 		a = pd.read_csv(fr"{path()}")
@@ -180,6 +261,11 @@ class Functions:
 				pass
 
 	def most_starred_user(self) -> pd.DataFrame:
+		"""
+		This function returns a dataframe with the most starred users.
+
+		:return: A dataframe
+		"""
 		df = pd.DataFrame(columns=["id", "user_name", "repo_name"])
 		for i in github_api.get_api_star_info():
 			if i.name == "copilot-preview":
@@ -196,6 +282,11 @@ class Functions:
 		return df.reset_index()
 
 	def most_starred_repo(self) -> pd.DataFrame:
+		"""
+		This function return most starred repo
+
+		:return: A dataframe
+		"""
 		df = pd.DataFrame(columns=["id", "user_name", "repo_name"])
 		for i in github_api.get_api_star_info():
 			if i.name == "copilot-preview":
@@ -213,6 +304,11 @@ class Functions:
 		return df
 
 	def gt_info(self) -> pd.DataFrame:
+		"""
+		This function return who doesn’t follow you back
+
+		:return: A dataframe
+		"""
 		df = self.user_followers_csv().merge(self.user_following_csv(), on="id", how="outer").drop_duplicates(
 			keep=False).fillna(0)
 		df = df[df["user_name_x"] == 0].drop(columns=["user_name_x"])
@@ -238,6 +334,9 @@ class MainMenu:
 			}
 
 		def show_menu(self):
+			"""
+			This function print main menu
+			"""
 			print("""
 menu
 1.followers
@@ -250,6 +349,9 @@ exit to q
     """)
 
 		def run(self):
+			"""
+			This function run main menu loop
+			"""
 			while True:
 				self.show_menu()
 				options = input("select options: ")
@@ -267,6 +369,10 @@ exit to q
 		pass
 
 	def main(self):
+		"""
+		This function create .env file if not exist
+		:return:
+		"""
 		try:
 			if os.path.isfile(f"{os.getcwd()}/env/.env"):
 				pass
